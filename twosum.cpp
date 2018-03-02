3ed32beb77434d361241efead48f7362e266ceed
Zhang Jin <zhangjin11@baidu.com>
2018-03-02 17:48:16 +0800

    two sum

---
+++ twosum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+class twosum {
+  public:
+	twosum();
+	int cal(int a, int b) {
+		return a + b;
+	}
+	~twosum();
+
+};
+int main () {
+	int n = 0 ; cout << "z" << endl;
+	return 0;
+}
